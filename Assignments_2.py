bbea6cd1b3d9967d300bb9a58612cf714c60d020
Fr0Zee <144336582+Fr0Zee@users.noreply.github.com>
2023-09-13 10:53:13 +0300

    Add files via upload

---
+++ Assignments_2.py
@@ -0,0 +1,11 @@
+Hours = float(input("Enter Hours : "))
+Rate = float(input("Enter Rate : "))
+print("Salary :",Hours * Rate)
+
+
+Celsius = float(input("Enter Celsius Temperature : "))
+print("Fahrenheit Temperature : ", Celsius * 9 / 5 + 32)
+
+
+Seconds = int(input("Enter seconds : "))
+print(Seconds,"seconds is",Seconds//3600,"hours,",(Seconds%3600)//60,"minutes,",(Seconds%3600)%60,"seconds")
